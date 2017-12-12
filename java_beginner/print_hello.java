ef7948ab7920901899485dd03052f29660160920
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2017-12-12 22:13:40 +0530

    Create print_hello.java

---
+++ java_beginner/print_hello.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Leap {
+    public void print(){
+        for(int i=0;i<5;i++){
+            System.out.println("Hello");
+        }
+    }
+    
+    public static void main(String[] args) {
+        Leap l=new Leap();
+        l.print();
+    }
+
+}
