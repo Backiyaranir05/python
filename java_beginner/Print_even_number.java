ae31ff81210d30c0c29676eddebcc9eb58ae6e9c
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2017-12-13 22:26:05 +0530

    Create Print_even_number.java

---
+++ java_beginner/Print_even_number.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Even{
+ public static void main(String args[]){
+  int i,firstNumber,lastNumber;
+  Scanner s=new Scanner(System.in);
+  firstNumber=s.nextInt();
+  lastNumber=s.nextInt();
+   for(i=firstNumber;i<=lastNumber;i++)
+   {
+    if(i%2==0){
+     System.out.println(i);
+    }
+   }
+}
+}
