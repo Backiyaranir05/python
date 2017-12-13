366c0807b6c0d460784caeba22d9f1866031b334
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2017-12-13 22:21:35 +0530

    Update Print_odd_number.java

--- java_beginner/Print_odd_number.java
+++ java_beginner/Print_odd_number.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.*;
 public class PrimeExample{
  public static void main(String args[]){
-  int i,firstNumber,secondNumber;
+  int i,firstNumber,lastNumber;
   Scanner s=new Scanner(System.in);
   firstNumber=s.nextInt();
-  secondNumber=s.nextInt();
-   for(i=firstNumber;i<=secondNumber;i++)
+  lastNumber=s.nextInt();
+   for(i=firstNumber;i<=lastNumber;i++)
    {
     if(i%2!=0){
      System.out.println(i);
