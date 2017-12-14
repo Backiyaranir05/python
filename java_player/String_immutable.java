a863a9c66bcd82f7c733cd3656ac9d4108a632ba
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2017-12-14 19:11:10 +0530

    Create String_immutable.java

---
+++ java_player/String_immutable.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		StringBuffer s1= new StringBuffer("Test");
+StringBuffer s2 = new StringBuffer("Test");
+if(s1.equals(s2)) {
+  System.out.println("True");
+} else {
+  System.out.println("False");
+}
+	}
+}
