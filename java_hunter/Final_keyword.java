a258ee6128b3736fbe5023e866773314a14121e2
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2017-12-14 18:54:17 +0530

    Create Final_keyword.java

---
+++ java_hunter/Final_keyword.java
@@ -0,0 +1,15 @@
+
+import java.lang.*;
+import java.io.*;
+class Final
+{
+  public final static print(){
+    final String str1="Hello friends... Am here...";
+    System.out.println(str1);
+  }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		GUVI g=new GUVI();
+        g.print();
+	}
+}
