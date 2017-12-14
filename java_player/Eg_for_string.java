2036f443b07b3fcce0971e8ae7c0981dddbe529c
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2017-12-14 19:13:33 +0530

    Create Eg_for_string.java

---
+++ java_player/Eg_for_string.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+  public void stringLength()
+  {
+    String str="Backiyarani05111997";
+    System.out.println(str.length());
+  }
+  
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		GUVI n=new GUVI();
+        n.stringLength();
+	}
+}
