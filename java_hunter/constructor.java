bc86700a3a94ac09e37c85f2248f90e14c8a05f1
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2017-12-14 18:56:30 +0530

    Create constructor.java

---
+++ java_hunter/constructor.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+  String name;
+  
+  GUVI(String ename)
+  {
+    name=ename;
+  }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      GUVI g.new GUVI("Shinchan");
+       System.out.println(g.name);
+		
+	}
+}
