c3e2f472438ac862f50073e67407655505949b3e
Backiyaranir05 <33174790+Backiyaranir05@users.noreply.github.com>
2018-01-16 13:58:54 +0530

    Create file_handling_type_convert.py

---
+++ python_hunter/file_handling_type_convert.py
@@ -0,0 +1,8 @@
+#filename=myfile.txt
+ffile = open("myfile.txt", "w+")
+for line in ffile:
+  splitted_line = line.split(' ')
+  for values in splitted_line:
+    value_as_int = int(values)
+    ffile.write(value_as_int)
+print(ffile.read())
